c9c0ec916cb9f7f77e4568089245a3063cf5b359
RohiT <rohit.s.0222@inspiria.edu.in>
2025-02-02 01:06:29 +0530

    --all done ig

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, { useState } from "react";
+import { Search } from "lucide-react";
+const SearchBar = ({ onSearch }: { onSearch: (city: string) => void }) => {
+  const [city, setCity] = useState("");
+  const handleSearch = () => {
+    onSearch(city);
+  };
+  return (
+    <div className="flex justify-center gap-2 items-center bg-slate-50 text-slate-900 w-[380px] rounded-md ">
+      <input
+        value={city}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setCity(e.target.value)
+        }
+        className="w-full bg-transparent px-6 py-4 outline-none"
+        type="text"
+        placeholder="Enter a city"
+      />
+      <button
+        type="submit"
+        onClick={handleSearch}
+        className="bg-slate-50 h-full text-slate-900 px-4 py-1 rounded-e-md hover:bg-slate-800 hover:text-white transition-all duration-300"
+      >
+        <Search />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,89 @@
+import "./WeatherCard.css";
+const WeatherCard = ({ weather }: { weather: any }) => {
+  return (
+    <div
+      id="box"
+      className={weather.elev_angle > 0 ? "bg-[#47c5ff]" : "bg-[#000040]"}
+    >
+      <div
+        id="moon"
+        className={weather.elev_angle > 0 ? "bg-[#ffff3c]" : "bg-[#f5f5dc]"}
+      >
+        <div className={weather.elev_angle > 0 ? "hidden" : ""}>
+          <div id="hole1" className="hole"></div>
+          <div id="hole2" className="hole"></div>
+          <div id="hole3" className="hole"></div>
+          <div id="hole4" className="hole"></div>
+          <div id="hole5" className="hole"></div>
+          <div id="hole6" className="hole"></div>
+          <div id="hole7" className="hole"></div>
+        </div>
+      </div>
+      <div
+        id="floor1"
+        className={`floor light ${
+          weather.elev_angle > 0 ? " bg-[#c7c7ce]" : " bg-[#404080]"
+        }`}
+      ></div>
+
+      <div
+        id="floor2"
+        className={`floor light ${
+          weather.elev_angle > 0 ? " bg-[#c7c7ce]" : " bg-[#404080]"
+        }`}
+      ></div>
+      <div
+        id="floor3"
+        className={`floor light ${
+          weather.elev_angle > 0 ? " bg-[#c7c7ce]" : " bg-[#404080]"
+        }`}
+      ></div>
+
+      <div
+        id="floor4"
+        className={`floor dark ${
+          weather.elev_angle > 0 ? " bg-[#6c6c8a]" : " bg-[#202060]"
+        }`}
+      ></div>
+      <div
+        id="floor5"
+        className={`floor dark ${
+          weather.elev_angle > 0 ? " bg-[#6c6c8a]" : " bg-[#202060]"
+        }`}
+      ></div>
+      <div
+        id="info"
+        className={`px-4 ${
+          weather.elev_angle > 0 ? "bg-white text-black" : "bg-black text-white"
+        }`}
+      >
+        <div className="flex items-center gap-2 justify-between">
+          <span id="temp">{weather.temp}°</span>
+
+          <span id="desc">{weather.weather.description}</span>
+        </div>
+
+        <div id="stats">
+          <span>🍃 Wind: {weather.wind_spd} Km/h</span>
+          <span>💧 Humidity: {weather.rh}%</span>
+        </div>
+      </div>
+      {weather.precip > 0 && (
+        <div>
+          <div id="drop1" className="drop"></div>
+          <div id="drop2" className="drop"></div>
+
+          <div id="drop3" className="drop"></div>
+
+          <div id="drop4" className="drop"></div>
+          <div id="drop5" className="drop away"></div>
+          <div id="drop6" className="drop away"></div>
+          <div id="drop7" className="drop away"></div>
+          <div id="drop8" className="drop away"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherCard;
